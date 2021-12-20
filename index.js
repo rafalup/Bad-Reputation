e5b78f1f181b69abd4474c62381f34b4118f5720
mazarafa <arruda.rafa.ra@gmail.com>
2021-12-20 14:23:37 -0400

    I made a connection with node js and postgres

--- index.js
+++ index.js
@@ -1,46 +1,77 @@
 let conn;
+const fs = require("fs");
+const fastcsv = require("fast-csv");
+const console = require("console");
 
 (async () => {
-    connect();
-    await createTable();
-    await insert();
-   /*  await showRows(); */
+  connect();
+  await createTable();
+  await insert();
+  await showRows();
 })();
 
 function connect() {
-    let { Client } = require('pg');
-    conn = new Client({
-        host: 'localhost',
-        database: 'vintage',
-        user: 'postgres',
-        password: 'senha',
-        port: 5432,
-    });
-    conn.connect();
+  let { Client } = require('pg');
+  conn = new Client({
+    host: 'localhost',
+    database: 'vintage',
+    user: 'postgres',
+    password: 'senha',
+    port: 5432,
+  });
+  conn.connect();
 }
 
 async function createTable() {
-    await conn.query(`
+  await conn.query(`
     create table if not exists people (
       id integer not null,
       name varchar,
       primary key (id)
     )
   `);
-    console.log('Table created');
+  console.log('Table created');
 }
 
 async function insert() {
-    let { rows } = await conn.query(
-        `insert into people (id, name) values (12, 'mario bross'), (21, 'luiza mel') returning *`
-    );
-    console.log(`Rows inserted: ${rows.length}`);
+    let stream = fs.createReadStream("arquivo.csv");
+    let csvData = [];
+    let csvStream = fastcsv
+    .parse()
+    .on("data", function(data) {
+        csvData.push(data);
+    })
+    .on("end", function() {
+        // remove the first line: header
+        csvData.shift();
+
+
+        const query =
+        "INSERT INTO people (id, name) VALUES ($1, $2)";
+
+        let i =1;
+    
+        csvData.forEach(row => {
+        conn.query(query, row, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log("inserted " + i + " row:", row);
+                i= i + 1;
+                //console.log(`Rows inserted: ${rows.length}`);
+            }
+            });
+           
+        });
+        
+    });
+    stream.pipe(csvStream);
     
 }
 
-/* async function showRows() {
-    let { rows } = await conn.query(`select * from people`);
-    for (const row of rows) {
-        console.log(row);
-    }
-} */
+async function showRows() {
+  let { rows } = await conn.query(`select * from people`);
+  for (const row of rows) {
+    console.log(row);
+  }
+} 
